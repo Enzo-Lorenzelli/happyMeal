a084daffb067308cd3fb6cdc64da2175060bf935
jeremy-gregoire <jeremy.gregoire@laplateforme.io>
2024-04-10 09:47:34 +0200

    Adding functionality and design for the home page.

--- js/home.js
+++ js/home.js
@@ -1,50 +1,74 @@
 const elemMainContainer = document.querySelector('#mainContainer');
+const elemFormSearchBar = document.querySelector('#formSearchBar');
+const elemSearchBar = document.querySelector('#searchBar');
+const elemSearchDataList = document.querySelector('#searchDataList');
 
 /**
- * @param {HTMLElement} parent
  * @param {string[]} recipeSteps
  */
-function createStepsList(parent, recipeSteps) {
-  const ul = document.createElement('ul');
+// function getStepsList(recipeSteps) {
+//   const tempContainer = document.createElement('template');
+//   tempContainer.innerHTML = `<div class="card"></div>`;
 
-  recipeSteps.forEach((recipeStep) => {
-    const li = document.createElement('li');
-    li.innerText = recipeStep;
-    ul.appendChild(li);
-  });
+//   const tempTitle = document.createElement('template');
+//   tempTitle.innerHTML = `<div class="card-header">Étapes</div>`;
 
-  parent.appendChild(ul);
-}
+//   const tempUl = document.createElement('template');
+//   tempUl.innerHTML = `<ul class="list-group list-group-flush"></ul>`;
+
+//   const elemContainer = tempContainer.content.firstElementChild;
+//   const elemTitle = tempTitle.content.firstElementChild;
+//   const elemUl = tempUl.content.firstElementChild;
+
+//   recipeSteps.forEach((recipeStep) => {
+//     const tempLi = document.createElement('template');
+//     tempLi.innerHTML = `<li class="list-group-item">${recipeStep}</li>`;
+//     elemUl.appendChild(tempLi.content.firstElementChild);
+//   });
+
+//   elemContainer.appendChild(elemTitle);
+//   elemContainer.appendChild(elemUl);
+
+//   return elemContainer;
+// }
 
 /**
- * @param {HTMLElement} parent
  * @param {object[]} recipeIngredients
  */
-function createIngredientsTable(parent, recipeIngredients) {
-  const table = document.createElement('table');
-  const thead = document.createElement('thead');
-  const tbody = document.createElement('tbody');
+// function getIngredientsTable(recipeIngredients) {
+//   const tempTable = document.createElement('template');
+//   tempTable.innerHTML = `<table class="table"></table>`;
 
-  thead.innerHTML = `
-    <tr>
-      <th>Nom de l'ingrédient</th>
-      <th>Quantité</th>
-    </tr>
-  `.trim();
+//   const tempTHead = document.createElement('template');
+//   tempTHead.innerHTML = `
+//     <tr>
+//       <th>Nom de l'ingrédient</th>
+//       <th>Quantité</th>
+//     </tr>
+//   `;
 
-  recipeIngredients.forEach((recipeIngredient) => {
-    const tr = document.createElement('tr');
-    tr.innerHTML = `
-      <td>${recipeIngredient.nom}</td>
-      <td>${recipeIngredient.quantite}</td>
-    `.trim();
-    tbody.appendChild(tr);
-  });
+//   const tempTBody = document.createElement('template');
+//   tempTBody.innerHTML = `<tbody></tbody>`;
 
-  table.appendChild(thead);
-  table.appendChild(tbody);
-  parent.appendChild(table);
-}
+//   const elemTable = tempTable.content.firstElementChild;
+//   const elemTHead = tempTHead.content.firstElementChild;
+//   const elemTBody = tempTBody.content.firstElementChild;
+
+//   recipeIngredients.forEach((recipeIngredient) => {
+//     const tempTr = document.createElement('template');
+//     tempTr.innerHTML = `
+//       <tr>
+//         <td>${recipeIngredient.nom}</td>
+//         <td>${recipeIngredient.quantite}</td>
+//       </tr>
+//     `;
+//     elemTBody.appendChild(tempTr.content.firstElementChild);
+//   });
+
+//   elemTable.appendChild(elemTHead);
+//   elemTable.appendChild(elemTBody);
+//   return elemTable;
+// }
 
 /**
  * @param {HTMLElement} parent
@@ -54,45 +78,123 @@ function createIngredientsTable(parent, recipeIngredients) {
  * @param {object[]} recipeIngredients
  * @param {string[]} recipeSteps
  */
-function createRecipeCard(
-  parent,
-  recipeName,
-  recipeCategory,
-  recipePreparationTime,
-  recipeIngredients,
-  recipeSteps
-) {
-  const card = document.createElement('div');
-  const basicInfo = document.createElement('div');
+// function displayRecipeCard(
+//   parent,
+//   recipeName,
+//   recipeCategory,
+//   recipePreparationTime,
+//   recipeIngredients,
+//   recipeSteps
+// ) {
+//   const tempCard = document.createElement('template');
+//   tempCard.innerHTML = `<div class="card"></div>`;
 
-  basicInfo.innerHTML = `
-    <div>
-      <p>Nom: ${recipeName}</p>
-      <p>Catégorie: ${recipeCategory}</p>
-      <p>Temps de préparation: ${recipePreparationTime}</p>
-    </div>
-  `.trim();
+//   const tempBody = document.createElement('template');
+//   tempBody.innerHTML = `<div class="card-body"></div>`;
 
-  card.appendChild(basicInfo);
-  createIngredientsTable(card, recipeIngredients);
-  createStepsList(card, recipeSteps);
+//   const tempBasicInfo = document.createElement('template');
+//   tempBasicInfo.innerHTML = `
+//     <div class="d-flex flex-row mb-3">
+//       <span class="card-text w-100 text-start">${recipeName}</span>
+//       <span class="card-text w-100 text-center">${recipeCategory}</span>
+//       <span class="card-text w-100 text-end">${recipePreparationTime}</span>
+//     </div>
+//   `;
 
-  parent.appendChild(card);
+//   const elemCard = tempCard.content.firstElementChild;
+//   const elemBody = tempBody.content.firstElementChild;
+//   const elemBasicInfos = tempBasicInfo.content.firstElementChild;
+//   const elemIngredientsTable = getIngredientsTable(recipeIngredients);
+//   const elemStepsList = getStepsList(recipeSteps);
+
+//   elemBody.appendChild(elemBasicInfos);
+//   elemBody.appendChild(elemIngredientsTable);
+//   elemBody.appendChild(elemStepsList);
+
+//   elemCard.appendChild(elemBody);
+//   parent.appendChild(elemCard);
+// }
+
+function getRandomElements(array, n) {
+  if (n > array.length) {
+    throw new Error('The number of elements requested is greater than the length of the array.');
+  }
+
+  const copy = array.slice();
+  const output = [];
+
+  for (let i = 0; i < n; i++) {
+    const randomIndex = Math.floor(Math.random() * copy.length);
+    output.push(copy[randomIndex]);
+    copy.splice(randomIndex, 1);
+  }
+
+  return output;
+}
+
+/**
+ * @param {SubmitEvent} e
+ */
+function onSubmit_elemFormSearchBar(e) {
+  e.preventDefault();
+  const redirectPage = '/index.html';
+  let searchValue = elemSearchBar.value;
+
+  if (searchValue === '') {
+    console.log('Not found');
+    return;
+  }
+
+  if (window.localStorage.getItem('searchValue') !== null) {
+    console.log('setting');
+    window.localStorage.setItem('searchValue', searchValue);
+    redirect(redirectPage);
+    return;
+  }
+
+  console.log('adding');
+  window.localStorage.setItem('searchValue', searchValue);
+  redirect(redirectPage);
+}
+
+function redirect(path) {
+  window.location.url = window.location.origin + path;
 }
 
 if (elemMainContainer) {
   getRecipes().then((data) => {
     let recipes = data.recettes;
 
-    let randomIndex = Math.floor(Math.random() * recipes.length);
-    let randomRecipe = recipes[randomIndex];
-    createRecipeCard(
-      elemMainContainer,
-      randomRecipe.nom,
-      randomRecipe.categorie,
-      randomRecipe.temps_preparation,
-      randomRecipe.ingredients,
-      randomRecipe.etapes
-    );
+    recipes.forEach((recipe) => {
+      const elemOption = document.createElement('option');
+      elemOption.value = recipe.nom;
+      elemOption.innerText = recipe.nom;
+      elemSearchDataList.appendChild(elemOption);
+    });
+
+    let randomSelections = getRandomElements(recipes, 6);
+
+    randomSelections.forEach((randomSelection) => {
+      const elemCol = document.createElement('div');
+      elemCol.classList.add('col');
+
+      elemCol.innerHTML = `
+        <div class="card h-100 shadow" style="border-color: #ff3333">
+          <img src="${randomSelection.image}" class="card-img-top" style="height: 250px; background-size: cover" alt="...">
+          <div class="card-body d-flex flex-column justify-content-between">
+            <div class="d-flex justify-content-between">
+              <h5 class="card-title fw-bold" style="color: #ff3333">${randomSelection.nom}</h5>
+              <span class="card-text">${randomSelection.categorie}</span>
+            </div>
+            <p class="card-text">Temps: ${randomSelection.temps_preparation}</p>
+            <a href="#" class="btn btn-primary border-0 custom-button custom-hover-button text-black" style="width: fit-content">Voir les détails</a>
+          </div>
+        </div>
+      `;
+
+      elemMainContainer.appendChild(elemCol);
+    });
+
+    elemFormSearchBar.addEventListener('submit', onSubmit_elemFormSearchBar);
   });
 }
