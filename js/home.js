74113e4b105d36144743ed69e21c051ad07db935
jeremy-gregoire <jeremy.gregoire@laplateforme.io>
2024-04-11 11:49:31 +0200

    Adding a modal for the recipe details

--- js/home.js
+++ js/home.js
@@ -1,200 +1,42 @@
-const elemMainContainer = document.querySelector('#mainContainer');
-const elemFormSearchBar = document.querySelector('#formSearchBar');
-const elemSearchBar = document.querySelector('#searchBar');
-const elemSearchDataList = document.querySelector('#searchDataList');
+const mainContainer = document.querySelector('#mainContainer');
 
 /**
- * @param {string[]} recipeSteps
+ * Adds a recipe card inside a parent element.
+ * @param {HTMLElement} parent The parent element to add the recipe card.
+ * @param {object} recipeData The data of the recipe.
+ * @param {number} index A looping index.
  */
-// function getStepsList(recipeSteps) {
-//   const tempContainer = document.createElement('template');
-//   tempContainer.innerHTML = `<div class="card"></div>`;
+function addRecipeCard(parent, recipeData, index) {
+  const btnShowId = `btnShowDetails${index}`;
+  const column = document.createElement('div');
 
-//   const tempTitle = document.createElement('template');
-//   tempTitle.innerHTML = `<div class="card-header">Étapes</div>`;
-
-//   const tempUl = document.createElement('template');
-//   tempUl.innerHTML = `<ul class="list-group list-group-flush"></ul>`;
-
-//   const elemContainer = tempContainer.content.firstElementChild;
-//   const elemTitle = tempTitle.content.firstElementChild;
-//   const elemUl = tempUl.content.firstElementChild;
-
-//   recipeSteps.forEach((recipeStep) => {
-//     const tempLi = document.createElement('template');
-//     tempLi.innerHTML = `<li class="list-group-item">${recipeStep}</li>`;
-//     elemUl.appendChild(tempLi.content.firstElementChild);
-//   });
-
-//   elemContainer.appendChild(elemTitle);
-//   elemContainer.appendChild(elemUl);
-
-//   return elemContainer;
-// }
-
-/**
- * @param {object[]} recipeIngredients
- */
-// function getIngredientsTable(recipeIngredients) {
-//   const tempTable = document.createElement('template');
-//   tempTable.innerHTML = `<table class="table"></table>`;
-
-//   const tempTHead = document.createElement('template');
-//   tempTHead.innerHTML = `
-//     <tr>
-//       <th>Nom de l'ingrédient</th>
-//       <th>Quantité</th>
-//     </tr>
-//   `;
-
-//   const tempTBody = document.createElement('template');
-//   tempTBody.innerHTML = `<tbody></tbody>`;
-
-//   const elemTable = tempTable.content.firstElementChild;
-//   const elemTHead = tempTHead.content.firstElementChild;
-//   const elemTBody = tempTBody.content.firstElementChild;
-
-//   recipeIngredients.forEach((recipeIngredient) => {
-//     const tempTr = document.createElement('template');
-//     tempTr.innerHTML = `
-//       <tr>
-//         <td>${recipeIngredient.nom}</td>
-//         <td>${recipeIngredient.quantite}</td>
-//       </tr>
-//     `;
-//     elemTBody.appendChild(tempTr.content.firstElementChild);
-//   });
-
-//   elemTable.appendChild(elemTHead);
-//   elemTable.appendChild(elemTBody);
-//   return elemTable;
-// }
-
-/**
- * @param {HTMLElement} parent
- * @param {string} recipeName
- * @param {string} recipeCategory
- * @param {string} recipePreparationTime
- * @param {object[]} recipeIngredients
- * @param {string[]} recipeSteps
- */
-// function displayRecipeCard(
-//   parent,
-//   recipeName,
-//   recipeCategory,
-//   recipePreparationTime,
-//   recipeIngredients,
-//   recipeSteps
-// ) {
-//   const tempCard = document.createElement('template');
-//   tempCard.innerHTML = `<div class="card"></div>`;
-
-//   const tempBody = document.createElement('template');
-//   tempBody.innerHTML = `<div class="card-body"></div>`;
-
-//   const tempBasicInfo = document.createElement('template');
-//   tempBasicInfo.innerHTML = `
-//     <div class="d-flex flex-row mb-3">
-//       <span class="card-text w-100 text-start">${recipeName}</span>
-//       <span class="card-text w-100 text-center">${recipeCategory}</span>
-//       <span class="card-text w-100 text-end">${recipePreparationTime}</span>
-//     </div>
-//   `;
-
-//   const elemCard = tempCard.content.firstElementChild;
-//   const elemBody = tempBody.content.firstElementChild;
-//   const elemBasicInfos = tempBasicInfo.content.firstElementChild;
-//   const elemIngredientsTable = getIngredientsTable(recipeIngredients);
-//   const elemStepsList = getStepsList(recipeSteps);
-
-//   elemBody.appendChild(elemBasicInfos);
-//   elemBody.appendChild(elemIngredientsTable);
-//   elemBody.appendChild(elemStepsList);
-
-//   elemCard.appendChild(elemBody);
-//   parent.appendChild(elemCard);
-// }
-
-function getRandomElements(array, n) {
-  if (n > array.length) {
-    throw new Error('The number of elements requested is greater than the length of the array.');
-  }
-
-  const copy = array.slice();
-  const output = [];
-
-  for (let i = 0; i < n; i++) {
-    const randomIndex = Math.floor(Math.random() * copy.length);
-    output.push(copy[randomIndex]);
-    copy.splice(randomIndex, 1);
-  }
-
-  return output;
-}
-
-/**
- * @param {SubmitEvent} e
- */
-function onSubmit_elemFormSearchBar(e) {
-  e.preventDefault();
-  const redirectPage = '/index.html';
-  let searchValue = elemSearchBar.value;
-
-  if (searchValue === '') {
-    console.log('Not found');
-    return;
-  }
-
-  if (window.localStorage.getItem('searchValue') !== null) {
-    console.log('setting');
-    window.localStorage.setItem('searchValue', searchValue);
-    redirect(redirectPage);
-    return;
-  }
-
-  console.log('adding');
-  window.localStorage.setItem('searchValue', searchValue);
-  redirect(redirectPage);
-}
-
-function redirect(path) {
-  window.location.url = window.location.origin + path;
-}
-
-if (elemMainContainer) {
-  getRecipes().then((data) => {
-    let recipes = data.recettes;
-
-    recipes.forEach((recipe) => {
-      const elemOption = document.createElement('option');
-      elemOption.value = recipe.nom;
-      elemOption.innerText = recipe.nom;
-      elemSearchDataList.appendChild(elemOption);
-    });
-
-    let randomSelections = getRandomElements(recipes, 6);
-
-    randomSelections.forEach((randomSelection) => {
-      const elemCol = document.createElement('div');
-      elemCol.classList.add('col');
-
-      elemCol.innerHTML = `
-        <div class="card h-100 shadow" style="border-color: #ff3333">
-          <img src="${randomSelection.image}" class="card-img-top" style="height: 250px; background-size: cover" alt="...">
-          <div class="card-body d-flex flex-column justify-content-between">
-            <div class="d-flex justify-content-between">
-              <h5 class="card-title fw-bold" style="color: #ff3333">${randomSelection.nom}</h5>
-              <span class="card-text">${randomSelection.categorie}</span>
-            </div>
-            <p class="card-text">Temps: ${randomSelection.temps_preparation}</p>
-            <a href="#" class="btn btn-primary border-0 custom-button custom-hover-button text-black" style="width: fit-content">Voir les détails</a>
-          </div>
+  column.classList.add('col');
+  column.innerHTML = `
+    <div class="card h-100 shadow" style="border-color: #ff3333">
+      <img src="${recipeData.image}" class="card-img-top" style="height: 250px; background-size: cover" alt="...">
+      <div class="card-body d-flex flex-column justify-content-between">
+        <div class="d-flex justify-content-between">
+          <h5 class="card-title fw-bold" style="color: #ff3333">${recipeData.nom}</h5>
+          <span class="card-text">${recipeData.categorie}</span>
         </div>
-      `;
+        <p class="card-text">Temps de préparation: ${recipeData.temps_preparation}</p>
+        <a id="${btnShowId}" href="#" data-bs-toggle="modal" data-bs-target="#detailsRecipe" class="btn btn-primary border-0 custom-button custom-hover-button text-black" style="width: fit-content">Voir les détails</a>
+      </div>
+    </div>
+  `;
 
-      elemMainContainer.appendChild(elemCol);
-    });
+  parent.appendChild(column);
+  const btnShow = document.querySelector(`#${btnShowId}`);
 
-    elemFormSearchBar.addEventListener('submit', onSubmit_elemFormSearchBar);
+  btnShow.addEventListener('click', () => {
+    clearModalDetailsRecipe();
+    showModalDetailsRecipe(recipeData);
   });
 }
+
+getRecipes().then((data) => {
+  const recipes = getRandomElements(data.recettes, 6);
+  recipes.forEach((recipe, i) => {
+    addRecipeCard(mainContainer, recipe, i);
+  });
+});
--- js/utilities/functions.js
+++ js/utilities/functions.js
@@ -1,1 +1,129 @@
+// Elements of the modal for the details of the recipe
+const modalDetailsRecipe = document.querySelector('#detailsRecipe');
+const modalDetailsRecipeName = modalDetailsRecipe.querySelector('#detailsRecipeName');
+const modalDetailsRecipeCategory = modalDetailsRecipe.querySelector('#detailsRecipeCategory');
+const modalDetailsRecipePreparationTime = modalDetailsRecipe.querySelector(
+  '#detailsRecipePreparationTime'
+);
+const modalDetailsRecipeContent = modalDetailsRecipe.querySelector('#detailsRecipeContent');
 
+// Elements for the search bar
+const searchBar = document.querySelector('#searchBar');
+const searchDataList = document.querySelector('#searchDataList');
+const searchButton = document.querySelector('#searchButton');
+
+/**
+ * Returns an array with n elements from an input array. This function avoids duplicates.
+ * @param {any[]} input An input array of values that will be used to generate the random output array.
+ * @param {number} n Number of random elements inside the array.
+ */
+function getRandomElements(input, n) {
+  if (n > input.length) {
+    throw new Error('The number of elements requested is greater than the length of the array.');
+  }
+
+  const copy = input.slice();
+  const output = [];
+
+  for (let i = 0; i < n; i++) {
+    const randomIndex = Math.floor(Math.random() * copy.length);
+    output.push(copy[randomIndex]);
+    copy.splice(randomIndex, 1);
+  }
+
+  return output;
+}
+
+/**
+ * Clear all information of the recipe modal.
+ */
+function clearModalDetailsRecipe() {
+  modalDetailsRecipeName.innerText = '';
+  modalDetailsRecipeCategory.innerText = '';
+  modalDetailsRecipePreparationTime.innerText = '';
+  modalDetailsRecipeContent.textContent = '';
+}
+
+/**
+ * Shows an error in the recipe modal.
+ * @param {string} message The error message.
+ */
+function showErrorModalDetailsRecipe(message) {
+  modalDetailsRecipeName.innerText = 'Error';
+  modalDetailsRecipeCategory.innerText = '';
+  modalDetailsRecipePreparationTime.innerText = '';
+  modalDetailsRecipeContent.innerHTML = `<p style="color: #ff3333">${message}<p>`;
+}
+
+/**
+ * Shows all information of a recipe in the recipe modal.
+ * @param {object} recipeData The current recipe.
+ */
+function showModalDetailsRecipe(recipeData) {
+  modalDetailsRecipeName.innerText = recipeData.nom;
+  modalDetailsRecipeCategory.innerText = recipeData.categorie;
+  modalDetailsRecipePreparationTime.innerText =
+    'Temps de préparation: ' + recipeData.temps_preparation;
+
+  modalDetailsRecipeContent.innerHTML = `
+    <h5 class="fw-bold" style="color: #ff3333">Ingrédients</h5>
+    <table class="table">
+      <tbody id="detailsRecipeIngredients"></tbody>
+    </table>
+    <h5 class="fw-bold" style="color: #ff3333">Étapes</h5>
+    <ol
+      class="list-group list-group-numbered list-group-flush"
+      id="detailsRecipeSteps"
+    ></ol>
+  `;
+
+  // Generates the table of ingredients.
+  const modalDetailsRecipeIngredients = modalDetailsRecipeContent.querySelector(
+    '#detailsRecipeIngredients'
+  );
+  recipeData.ingredients.forEach((ingredient) => {
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+      <td>${ingredient.nom}</td>
+      <td>${ingredient.quantite}</td>
+    `;
+
+    modalDetailsRecipeIngredients.appendChild(row);
+  });
+
+  // Generates the list of steps.
+  const modalDetailsRecipeSteps = modalDetailsRecipeContent.querySelector('#detailsRecipeSteps');
+  recipeData.etapes.forEach((step) => {
+    const row = document.createElement('li');
+    row.classList.add('list-group-item');
+    row.innerText = step;
+    modalDetailsRecipeSteps.appendChild(row);
+  });
+}
+
+getRecipes().then((data) => {
+  data.recettes.forEach((recipe) => {
+    const option = document.createElement('option');
+    option.value = recipe.nom;
+    option.innerText = recipe.nom;
+    searchDataList.appendChild(option);
+  });
+
+  searchButton.addEventListener('click', () => {
+    clearModalDetailsRecipe();
+
+    if (searchBar.value === '') {
+      showErrorModalDetailsRecipe('La recherche est vide !');
+      return;
+    }
+
+    const find = data.recettes.filter((recipe) => recipe.nom === searchBar.value);
+    if (find.length === 0) {
+      showErrorModalDetailsRecipe('Le résultat introuvable !');
+      return;
+    }
+
+    showModalDetailsRecipe(find[0]);
+  });
+});
