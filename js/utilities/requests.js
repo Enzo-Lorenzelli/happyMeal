6cc5d7f3704e3f974e8f46814c5b6453397f5b8a
jeremy-gregoire <jeremy.gregoire@laplateforme.io>
2024-04-14 15:03:05 +0200

    Fix redirecting 3

--- js/utilities/requests.js
+++ js/utilities/requests.js
@@ -1,6 +1,6 @@
 async function getRecipes() {
   try {
-    const response = await fetch(window.location.origin + 'happyMeal/data.json');
+    const response = await fetch(window.location.origin + '/happyMeal/data.json');
 
     if (!response.ok || response.status !== 200) {
       console.error(`[${response.status}] Impossible to fetch recipes at '${response.url}'`);
